2ff213388ab09c392efe8798306504fe9d572323
Teus Toscano <ssstoscano@gmail.com>
2016-12-21 21:59:17 -0200

    Created a change letters color behavior when click the boxe with title

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 document.getElementById('heading').innerHTML = localStorage['title'] || 'Start Here';
 document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second';
 
+var count = 0;
+
 setInterval(function(){
 	localStorage['title'] = document.getElementById('heading').innerHTML;
 	localStorage['text'] = document.getElementById('content').innerHTML;
 }, 1000);
+
+function changeColorFunction (argument) {
+	if((count % 2) == 0){
+		document.getElementById('content').setAttribute("class", "lettersColor2");
+		document.getElementById('heading').setAttribute("class", "lettersColor2");
+		document.getElementById('footer').setAttribute("class", "lettersColor2");
+		document.getElementById('boxed').setAttribute("class", "lettersColor2");
+		count++;
+	}else if((count % 2) != 0){
+		document.getElementById('content').setAttribute("class", "lettersColor1");
+		document.getElementById('heading').setAttribute("class", "lettersColor1");
+		document.getElementById('footer').setAttribute("class", "lettersColor1");
+		document.getElementById('boxed').setAttribute("class", "lettersColor1");
+		count++;
+	}
+}
